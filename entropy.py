c6948304ff3bac421983329555fc811f87aabec7
yashgupta228 <yashguptaa14@gmail.com>
2026-02-21 23:04:24 +0530

    Update entropy.py

--- entropy.py
+++ entropy.py
@@ -67,7 +67,7 @@ if 'tree' in st.session_state:
     inputs = {col: st.selectbox(col, df[col].unique()) for col in features}
     if st.button("Predict"):
        st.write(f"Result: {predict(st.session_state['tree'], inputs)}")
-        fig, ax = plt.subplots()
+    fig, ax = plt.subplots()
 colors = {"yes": "green", "no": "red"}
 
 for outcome in df[target_col].unique():
