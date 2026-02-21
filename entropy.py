1e2995665346009f83b0e2f2e1f9c5164362cd15
yashgupta228 <yashguptaa14@gmail.com>
2026-02-21 22:44:26 +0530

    Update entropy.py

--- entropy.py
+++ entropy.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import math
+import matplotlib.pyplot as plt
 
 def entropy(col):
     values, counts = np.unoique(col, return_counts=True)
