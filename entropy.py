a6a5d0f2dda0bc2d83573e97d74a43544c75bf78
yashgupta228 <yashguptaa14@gmail.com>
2026-02-22 15:44:52 +0530

    Update entropy.py

--- entropy.py
+++ entropy.py
@@ -21,12 +21,14 @@ def info_gain(df, attr, target):
     return  total_entropy  -  weighted_entropy
 
 def id3(df,  target,attrs):
-    if len(df[target].unique()) ==1:
+    if len(df[target].unique()) == 1:
         return df[target].iloc[0]
     if not attrs:
         return df[target].mode()[0]
+        
     best = max(attrs,key=lambda a: info_gain(df, a, target))
     tree= {best: {}}
+    
     for val in df[best].unique():
          sub_df = df[df[best] == val]
          tree[best][val] = id3(sub_df, target, [a for a in attrs if a != best])
